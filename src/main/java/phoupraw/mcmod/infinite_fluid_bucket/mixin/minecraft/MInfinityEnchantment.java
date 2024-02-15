6c7f90fae488b7727e68de9e4c1ddfd766c18af9
Phoupraw <467934387@qq.com>
2024-02-16 00:21:08 +0800

    允许附魔无限，不依赖《通用附魔》

---
+++ src/main/java/phoupraw/mcmod/infinite_fluid_bucket/mixin/minecraft/MInfinityEnchantment.java
@@ -0,0 +1,20 @@
+package phoupraw.mcmod.infinite_fluid_bucket.mixin.minecraft;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.InfinityEnchantment;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(InfinityEnchantment.class)
+abstract class MInfinityEnchantment extends Enchantment {
+    protected MInfinityEnchantment(Rarity weight, EnchantmentTarget target, EquipmentSlot[] slotTypes) {
+        super(weight, target, slotTypes);
+    }
+    @Override
+    public boolean isAcceptableItem(ItemStack stack) {
+        return super.isAcceptableItem(stack) || stack.isOf(Items.BUCKET) || stack.isOf(Items.WATER_BUCKET);
+    }
+}
