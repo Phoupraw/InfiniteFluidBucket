02b9bff677498de1e0b8054a84ba4566eecade84
Phoupraw <467934387@qq.com>
2024-02-17 21:39:45 +0800

    中文

--- src/main/kotlin/phoupraw/mcmod/infinite_fluid_bucket/datagen/InfiniteFluidBucketDataGen.kt
+++ src/main/kotlin/phoupraw/mcmod/infinite_fluid_bucket/datagen/InfiniteFluidBucketDataGen.kt
@@ -10,6 +10,7 @@ import phoupraw.mcmod.infinite_fluid_bucket.CONFIG
 import phoupraw.mcmod.infinite_fluid_bucket.ID
 import phoupraw.mcmod.infinite_fluid_bucket.config.InfiniteFluidBucketConfig
 import java.util.concurrent.CompletableFuture
+import kotlin.reflect.KProperty
 
 @Suppress("unused")
 object InfiniteFluidBucketDataGen : DataGeneratorEntrypoint {
@@ -20,6 +21,13 @@ object InfiniteFluidBucketDataGen : DataGeneratorEntrypoint {
         pack.addProvider(::FluidTagGen)
         pack.addProvider(::ItemTagGen)
     }
+}
+
+private val CFG = "yacl3.config.${CONFIG.id()}"
+private fun FabricLanguageProvider.TranslationBuilder.add(property: KProperty<*>, name: String, desc: String) {
+    val configName = "$CFG.${property.name}"
+    add(configName, name)
+    add("$configName.desc", desc)
 }
 
 private class ChineseGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(dataOutput, "zh_cn") {
@@ -35,27 +43,11 @@ private class ChineseGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(
             可以用附魔台或铁砧给桶附魔无限。
             每个物品可以从配置文件或《模组菜单》单独开关。
             可以只安装在服务端，只不过客户端可能有一点同步问题。
-            无限水桶
-            - 可以无限放置水。
-            - 可以无限向炼药锅填充水。
-            - 在发射器中，可以无限放置水。
-            - 可以向模组的流体储罐无限提供水，每次提供一桶水。
-            - 不可以捕鱼。
-            无限铁桶
-            - 可以无限舀起并销毁流体源。
-            - 可以无限清空炼药锅中的流体和细雪。
-            - 在发射器中，可以无限舀起并销毁流体源。
-            - 可以被模组的流体储罐无限输入流体，每次可以输入一桶量的流体。输入的流体会被销毁。
-            - 不可以挤奶。
-            无限奶桶
-            - 可以无限饮用。
             §l你知道吗§r
             本模组灵感来源于《夸克》中的无限水桶。
         """.trimIndent())
-        val cfg = "yacl3.config.${CONFIG.id()}"
-        b.add("$cfg.category.${InfiniteFluidBucketConfig.CATEGORY}", "设置")
-        b.add("$cfg.${InfiniteFluidBucketConfig::waterBucket.name}", "水桶")
-        b.add("$cfg.${InfiniteFluidBucketConfig::waterBucket.name}.desc", """
+        b.add("$CFG.category.${InfiniteFluidBucketConfig.CATEGORY}", "设置")
+        b.add(InfiniteFluidBucketConfig::waterBucket, "水桶", """
             启用时，可以用附魔台或铁砧给水桶附魔无限。
             §l无限水桶§r
             - 手持时，可以无限放置水。
@@ -64,8 +56,7 @@ private class ChineseGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(
             - 可以向模组的流体储罐无限提供水，每次提供一桶水。
             - 不可以捕鱼。
         """.trimIndent())
-        b.add("$cfg.${InfiniteFluidBucketConfig::emptyBucket.name}", "空桶")
-        b.add("$cfg.${InfiniteFluidBucketConfig::emptyBucket.name}.desc", """
+        b.add(InfiniteFluidBucketConfig::emptyBucket, "空桶", """
             启用时，可以用附魔台或铁砧给铁桶附魔无限。
             §l无限空桶§r
             - 手持时，可以无限舀起并销毁流体源。
@@ -74,11 +65,28 @@ private class ChineseGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(
             - 可以被模组的流体储罐无限输入流体，每次可以输入一桶量的流体，并且销毁。
             - 不可以挤奶。
         """.trimIndent())
-        b.add("$cfg.${InfiniteFluidBucketConfig::milkBucket.name}", "奶桶")
-        b.add("$cfg.${InfiniteFluidBucketConfig::milkBucket.name}.desc", """
+        b.add(InfiniteFluidBucketConfig::milkBucket, "奶桶", """
             启用时，可以用附魔台或铁砧给奶桶附魔无限。
             §l无限奶桶§r
             - 可以无限饮用。
+        """.trimIndent())
+        b.add(InfiniteFluidBucketConfig::glassBottle, "空瓶", """
+            启用时，可以用附魔台或铁砧给玻璃瓶附魔无限。
+            §l无限空瓶§r
+            - 可以无限从炼药锅无限舀起并销毁水。
+            - 可以无限收集并销毁龙息。
+            - 可以无限从装满蜂蜜的蜂巢和蜂箱收集并销毁蜂蜜。
+            - 可以被模组的流体储罐无限输入流体，每次可以输入一瓶量的流体。输入的流体会被销毁。
+            - 不能用于合成。
+        """.trimIndent())
+        b.add(InfiniteFluidBucketConfig::waterPotion, "水瓶", """
+            启用时，可以用附魔台或铁砧给水瓶附魔无限。
+            §l无限水瓶§r
+            - 可以无限向炼药锅倒水。
+            - 可以无限把泥土变成泥巴。
+            - 可以向模组的流体储罐无限提供水，每次提供一瓶水。
+            - 不会在合成中消耗。
+            - 不能用于酿造。
         """.trimIndent())
     }
 }
@@ -95,27 +103,12 @@ private class EnglishGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(
             Enchant buckets by enchanting table or anvil.
             Each item can be toggled independently by config file or Mod Menu.
             Can be installed only at server side, just client might have a few sync bugs.
-            Infinity Water Bucket
-            - Infinitely place water.
-            - Infinitely fill cauldron.
-            - Infinitely place water in dispenser.
-            - Infinitely provide water to fluid tank of mod. Each time one bucket of water.
-            - Can't fish.
-            Infinity Empty Bucket
-            - Infinitely scoop and void fluid.
-            - Infinitely empty fluid in cauldron, including powder snow.
-            - Infinitely scoop and void fluid in dispenser.
-            - Can be inserted with fluid by fluid tank of mod. Each time one bucket of fluid. Inserted fluid is voided.
-            - Can't milk.
-            Infinity Milk Bucket
-            - Can be infinitely drunk.
             §lTrivia§r
             This mod is inspired by infinite water bucket of Quark mod.
         """.trimIndent())
         val cfg = "yacl3.config.${CONFIG.id()}"
         b.add("$cfg.category.${InfiniteFluidBucketConfig.CATEGORY}", "Settings")
-        b.add("$cfg.${InfiniteFluidBucketConfig::waterBucket.name}", "Water Bucket")
-        b.add("$cfg.${InfiniteFluidBucketConfig::waterBucket.name}.desc", """
+        b.add(InfiniteFluidBucketConfig::waterBucket, "Water Bucket", """
             When enabled, water bucket can be enchanted with infinity.
             §lInfinity Water Bucket§r
             - Infinitely place water.
@@ -124,8 +117,7 @@ private class EnglishGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(
             - Infinitely provide water to fluid tank of mod. Each time one bucket of water.
             - Can't fish.
         """.trimIndent())
-        b.add("$cfg.${InfiniteFluidBucketConfig::emptyBucket.name}", "Empty Bucket")
-        b.add("$cfg.${InfiniteFluidBucketConfig::emptyBucket.name}.desc", """
+        b.add(InfiniteFluidBucketConfig::emptyBucket, "Empty Bucket", """
             When enabled, bucket can be enchanted with infinity.
             §lInfinity Empty Bucket§r
             - Infinitely scoop and void fluid.
@@ -134,8 +126,7 @@ private class EnglishGen(dataOutput: FabricDataOutput) : FabricLanguageProvider(
             - Can be inserted with fluid by fluid tank of mod. Each time one bucket of fluid. Inserted fluid is voided.
             - Can't milk.
         """.trimIndent())
-        b.add("$cfg.${InfiniteFluidBucketConfig::milkBucket.name}", "Milk Bucket")
-        b.add("$cfg.${InfiniteFluidBucketConfig::milkBucket.name}.desc", """
+        b.add(InfiniteFluidBucketConfig::milkBucket, "Milk Bucket", """
             When enabled, milk bucket can be enchanted with infinity.
             §lInfinity Milk Bucket§r
             - Can be infinitely drunk.
