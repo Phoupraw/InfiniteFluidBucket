90f41412a0d19b6faacc12691e0329ce302f4148
Phoupraw <467934387@qq.com>
2024-04-15 15:17:01 +0800

    修复：转运量可以小于1桶

--- src/main/kotlin/phoupraw/mcmod/infinite_fluid_bucket/transfer/InfEmptyFluidStorage.kt
+++ src/main/kotlin/phoupraw/mcmod/infinite_fluid_bucket/transfer/InfEmptyFluidStorage.kt
@@ -6,11 +6,10 @@ import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant
 import net.fabricmc.fabric.api.transfer.v1.storage.base.InsertionOnlyStorage
 import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext
-import kotlin.math.min
 
 open class InfEmptyFluidStorage(override val capacity: Long) : SingleSlotStorage<FluidVariant>, InsertionOnlyStorage<FluidVariant> {
     override fun extract(resource: FluidVariant, maxAmount: Long, transaction: TransactionContext): Long = super.extract(resource, maxAmount, transaction)
-    override fun insert(resource: FluidVariant, maxAmount: Long, transaction: TransactionContext): Long = min(maxAmount, capacity)
+    override fun insert(resource: FluidVariant, maxAmount: Long, transaction: TransactionContext): Long = if (maxAmount >= capacity) capacity else 0
     override val resourceBlank: Boolean @JvmName("isResourceBlank") get() = true
     override val resource: FluidVariant get() = FluidVariant.blank()
     override val amount: Long get() = 0
--- src/main/kotlin/phoupraw/mcmod/infinite_fluid_bucket/transfer/InfFluidBucketStorage.kt
+++ src/main/kotlin/phoupraw/mcmod/infinite_fluid_bucket/transfer/InfFluidBucketStorage.kt
@@ -6,10 +6,9 @@ import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant
 import net.fabricmc.fabric.api.transfer.v1.storage.base.ExtractionOnlyStorage
 import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext
-import kotlin.math.min
 
 open class InfFluidBucketStorage(override val resource: FluidVariant, override val capacity: Long) : SingleSlotStorage<FluidVariant>, ExtractionOnlyStorage<FluidVariant> {
-    override fun extract(resource: FluidVariant, maxAmount: Long, transaction: TransactionContext): Long = if (this.resource == resource) min(maxAmount, amount) else 0
+    override fun extract(resource: FluidVariant, maxAmount: Long, transaction: TransactionContext): Long = if (this.resource == resource && amount >= maxAmount) amount else 0
     override val resourceBlank: Boolean @JvmName("isResourceBlank") get() = false
     override val amount: Long get() = capacity
 }
